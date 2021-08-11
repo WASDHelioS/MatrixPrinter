c55a5a34cf5ba924f106786c1b69bca030f815b7
wsadhelios <wsadhelios@gmail.com>
2021-08-11 12:17:27 +0200

    Add proj

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+namespace MatrixPrinter
+{
+    class Program
+    {
+        private static char[,] matrix = new char[5, 3]
+        {
+            {'*','*','*'},
+            {'*','0','*'},
+            {'*','0','*'},
+            {'*','0','*'},
+            {'*','*','*'}
+        };
+        static void Main(string[] args)
+        {
+            for(int col = 0; col < matrix.GetLength(0);col++)
+            {
+                string prt = String.Join("  ", Enumerable.Range(0, matrix.GetLength(1)).Select(x => matrix[col, x]).ToArray()).Trim(' ');
+                Console.WriteLine(prt);
+            }
+        }
+    }
+}
